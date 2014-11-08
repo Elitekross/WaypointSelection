a859a49be25d5cb58a26eb729305fb2baf83280c
Elitekross <Ellitekross@yahoo.com>
2014-11-08 16:12:38 -0600

    Most recent update
    Waypoint, WaypointList definitions, route creation

--- WaypointSelection/src/classes/ProjectMain.java
+++ WaypointSelection/src/classes/ProjectMain.java
@@ -7,23 +7,52 @@ public class ProjectMain {
 	/**
 	 * @param args
 	 */
-	WaypointList local = new WaypointList();
+	static WaypointList local = new WaypointList();
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Waypoint a = new Waypoint(0.0, 0.0, 0, 0);
+		Waypoint b = new Waypoint(1.0, 0.0, 0, 0);
+		
+		Waypoint a1 = new Waypoint(2.0, 1.5, 1, .5);
+		Waypoint b1 = new Waypoint(1.5, 1.5, 1, .7);
+		Waypoint c1 = new Waypoint(1.0, 1.0, 1, .6);
+		
+		Waypoint a2 = new Waypoint(1.9, 1.6, 2, .7);
+		Waypoint b2 = new Waypoint(1.9, 1.6, 2, .6);
+		
+		local.waypointList.add(a1);
+		local.waypointList.add(a2);
+		local.waypointList.add(b1);
+		local.waypointList.add(b2);
+		local.waypointList.add(c1);
+		
+		local.display();
+		System.out.println();
+		local.getNearest(a, true).display();
+		System.out.println();
+		local.display();
+		//WaypointList route = createRoute(3, 1, a, b);
+		//System.out.println("Length of route: " + route.lengthAlong());
+		//route.display();
 
 	}
 
-	public WaypointList createRoute(double dist, int type, Waypoint start, Waypoint end){
+		public static WaypointList createRoute(double dist, int type, Waypoint start, Waypoint end){
 		WaypointList result = new WaypointList();
-		WaypointList LocalType = new WaypointList(local.pullList(type));
-		result.waypointList.add(start);
-		result.waypointList.add(end);
-		while (result.lengthAlong() < (0.95 * dist)) {
-			result.waypointList.add(result.waypointList.size() - 1, LocalType.getPriority());
+		WaypointList LocalType = local.pullType(1);
+		result.add(start);
+		result.add(end);
+		double resultLength = result.lengthAlong();
+		while (resultLength < (0.8 * dist)) {
+			result.add(result.size() - 1, LocalType.getPriority());
+			resultLength = result.lengthAlong();
+			if (LocalType.size()== 0) {
+				break;
+			}
 		}
-		while (result.lengthAlong() > (1.05 * dist)) {
-			result.waypointList.remove(result.waypointList.size() - 2);
+		while (result.lengthAlong() > (1.2* dist)) {
+			result.remove(result.size() - 2);
+			resultLength = result.lengthAlong();
 		}
 		return result;
 	}
--- WaypointSelection/src/classes/WaypointList.java
+++ WaypointSelection/src/classes/WaypointList.java
@@ -4,26 +4,37 @@ import java.util.ArrayList;
 
 public class WaypointList {
 	ArrayList<Waypoint> waypointList;
-	
-	public ArrayList<Waypoint> pullList(int type) {
-		ArrayList<Waypoint> result = new ArrayList<Waypoint>();
+	//creates a WaypointList of type "type" from a given list
+	public WaypointList pullType(int type) {
+		WaypointList result = new WaypointList();
 		if (type < 3 && type >= 0) {
 			for (int i = 0; i < this.waypointList.size(); i++){
 				if (this.waypointList.get(i).type == type) {
-					result.add(this.waypointList.get(i));
+					result.add(this.get(i));
 				}
 			}
 		}
 		return result;
 	}
+	//create a WaypointList of points within a certain radius from a given list
+	public WaypointList pullRadius(double radius, Waypoint a){
+		WaypointList result = new WaypointList();
+		for (int i = 0; i < this.size(); i++){
+			if (Waypoint.distance(a, this.get(i)) <= radius){
+				result.add(this.get(i));
+			}
+		}
+		return result;
+	}
+	//outputs the element of the highest weight, and removes it from the list
 	public Waypoint getPriority() {
 		int index = 0;
 		double maxPriority = 0;
 		Waypoint result;
-		for (int i = 0; i < this.waypointList.size(); i++){
-			if (maxPriority < this.waypointList.get(i).weight) {
+		for (int i = 0; i < this.size(); i++){
+			if (maxPriority < this.get(i).weight) {
 				index = i;
-				maxPriority = this.waypointList.get(i).weight;
+				maxPriority = this.get(i).weight;
 			}
 		}
 		result = this.waypointList.get(index);
@@ -31,12 +42,66 @@ public class WaypointList {
 		return result;
 	}
 	
+	//outputs the nearest element to input, and removes it from the list
+	public Waypoint getNearest(Waypoint input){
+		Waypoint result = input.findNearest(this);
+		int index = this.indexOf(result);
+		this.remove(index);
+		return result;
+	}
+	//outputs the nearest element with the highest priority, and removes it from the list. (margin of  5%)
+	public Waypoint getNearest(Waypoint input, boolean flag){
+		Waypoint result = input;
+		WaypointList field = this;
+		if(!flag){
+			result = this.getNearest(input);
+		}
+		else {
+			result = input.findNearest(this);
+			double radius = (Waypoint.distance(input, result) * .05);
+			result = input.findPriority(radius, this);
+			int index = this.indexOf(result);
+			this.remove(index);
+		}
+		return result;
+	}
+	
+	//the following replicate ArrayList functions for ease
+	public int indexOf(Waypoint a){
+		return this.waypointList.indexOf(a);
+	}
+	public void add(Waypoint a){
+		this.waypointList.add(a);
+	}
+	public void add(int index, Waypoint a){
+		this.waypointList.add(index, a);
+	}
+	public Waypoint get(int index){
+		return this.waypointList.get(index);
+	}
+	public int size(){
+		return this.waypointList.size();
+	}
+	public void set(int index, Waypoint a){
+		this.waypointList.set(index, a);
+	}
+	public void remove(int index){
+		this.waypointList.remove(index);
+	}
+	
+	//find the length along the points in a WaypointList
 	public double lengthAlong(){
 		double length = 0.0;
 		for (int i = 1; i < this.waypointList.size(); i++) {
-			length = Waypoint.distance(this.waypointList.get(i), this.waypointList.get(i - 1));
+			length += Waypoint.distance(this.waypointList.get(i), this.waypointList.get(i - 1));
 		}
 		return length;
+	}
+	//display all waypoints in the WaypointList
+	public void display(){
+		for(int i = 0; i < this.waypointList.size(); i++){
+			this.waypointList.get(i).display();
+		}
 	}
 	
 	public WaypointList(ArrayList<Waypoint> input) {
