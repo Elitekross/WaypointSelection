ef9ee8b90a4818693067a8c54e554c97448663ca
Elitekross <Ellitekross@yahoo.com>
2014-12-06 18:13:45 -0600

    Dijkstra's
    Got dijkstra's to work and integrated it

---
+++ WaypointSelection/src/classes/WaypointViewer.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class WaypointViewer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GUI gui = new GUI();
+		gui.setVisible(true);
+
+	
+	}
+}
